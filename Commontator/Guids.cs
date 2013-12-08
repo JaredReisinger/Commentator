c0b07fc487a781d2445cf809f639513a9fd65a36
Jared Reisinger <jaredreisinger@hotmail.com>
2013-12-08 12:03:55 -0800

    Empty VS Package project

---
+++ Commontator/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Spudnoggin.Commontator
+{
+    static class GuidList
+    {
+        public const string guidCommontatorPkgString = "8c308e81-b885-44b1-b2c6-f8b3e14cc6c3";
+        public const string guidCommontatorCmdSetString = "757260c9-0fbd-4ce3-a50b-2e0803d1d766";
+
+        public static readonly Guid guidCommontatorCmdSet = new Guid(guidCommontatorCmdSetString);
+    };
+}
---
+++ Commontator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Commontator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Spudnoggin")]
+[assembly: AssemblyProduct("Commontator")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Commontator_IntegrationTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010041f9c83afac00999bbf7b22613f1aba6a40d875d27b129554834038ec00eb11bd487460975afe50fdb0bf68d3dc7328fa5e7d42efa2a676cbf9415200b7a0857de6d09050dcd2e8abfd8a2f92c02b7d2cb3cc931889c4925c004074ea651cd3b11262653f27a3fa99f3071b28427b2cc0cfb92a17f32073108df05f23c058de3")]
+[assembly: InternalsVisibleTo("Commontator_UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010041f9c83afac00999bbf7b22613f1aba6a40d875d27b129554834038ec00eb11bd487460975afe50fdb0bf68d3dc7328fa5e7d42efa2a676cbf9415200b7a0857de6d09050dcd2e8abfd8a2f92c02b7d2cb3cc931889c4925c004074ea651cd3b11262653f27a3fa99f3071b28427b2cc0cfb92a17f32073108df05f23c058de3")]
